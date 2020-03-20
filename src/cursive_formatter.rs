fec25764b217e23e9fed92d8fb14f81cdd6327c2
Lucas Taylor <lucas@perfectlunacy.com>
2020-03-19 21:50:57 -0700

    Fixes color string formatting

--- src/cursive_formatter.rs
+++ src/cursive_formatter.rs
@@ -2,6 +2,7 @@ use regex::{Regex, Match};
 use cursive::utils::markup::StyledString;
 use cursive::theme::{Color, BaseColor, Effect, Style};
 use cursive::views::TextView;
+use log::{info, trace};
 use crate::cursive_formatter::TextMode::{HI_COLOR, LO_COLOR};
 
 pub fn format(s: &str) -> StyledString {
@@ -11,13 +12,32 @@ pub fn format(s: &str) -> StyledString {
     if vt100_esc_codes.is_empty() {
         styled.append(StyledString::plain(s));
     } else {
-        let mut head = 0;
+        let mut style_to_apply:Match = vt100_esc_codes.first().unwrap().clone();
+
+        if style_to_apply.start() > 0 {
+            let content = &s[0..style_to_apply.start()];
+            let to_add = StyledString::plain(content);
+            info!("{:?}", to_add);
+            styled.append(to_add);
+        }
+
+        let mut head = style_to_apply.end();
         for esc_code_match in vt100_esc_codes {
+            if esc_code_match == style_to_apply { continue; } // This will only be true the first time through. I couldn't get .skip to work.
+
             let content = &s[head..esc_code_match.start()];
-            let style = style_for_vt100_code(esc_code_match.as_str());
+            let style = style_for_vt100_code(style_to_apply.as_str());
+            let to_add = StyledString::styled(content, style);
+            info!("{:?}", to_add);
+            styled.append(to_add);
 
             head = esc_code_match.end();
+            style_to_apply = esc_code_match; // Whatever we just matched will be enforced for the next block of the string
+        }
 
+        if head < s.len() {
+            let content = &s[head..s.len()];
+            let style = style_for_vt100_code(style_to_apply.as_str());
             styled.append(StyledString::styled(content, style));
         }
     }
@@ -37,7 +57,8 @@ pub fn find_vt100s(s: &str) -> Vec<Match> {
 
 pub fn style_for_vt100_code(esc_code: &str) -> Style {
     let cmd = esc_code.replace("\u{1B}", "");
-    let color_match = Regex::new(r"[\d;]*m").unwrap().find(&cmd);
+    info!("Color cmd: {}", cmd);
+    let color_match = Regex::new(r"[\d;]*m").unwrap().find_iter(&cmd).last();
 
     match color_match {
         Some(clr_code) => style_for_color_code(clr_code.as_str().to_string().replace("m", "").as_str()),
@@ -56,24 +77,21 @@ pub fn style_for_color_code(clr_code: &str) -> Style {
 
     let mut mode = TextMode::LO_COLOR;
 
-    print!("{:?} -> ", cmds);
-
     for color_cmd in cmds.iter().map(|c| c.parse::<u32>().unwrap()) {
         let next_style: Style = match color_cmd {
             1 => { style.combine(Effect::Bold); style }
             4 => { style.combine(Effect::Underline); style }
-            30..=37 => { mode = LO_COLOR; Style::from(Color::Light(BaseColor::from((color_cmd - 30) as u8))) },
-            40..=47 => { mode = LO_COLOR; Style::from(Color::Light(BaseColor::from((color_cmd - 40) as u8))) },
+            30..=37 => { mode = LO_COLOR; Style::from(Color::Dark(BaseColor::from((color_cmd - 30) as u8))) },
+            40..=47 => { mode = LO_COLOR; Style::from(Color::Dark(BaseColor::from((color_cmd - 40) as u8))) },
             38 => { mode = HI_COLOR; style }
-            90..=97 => { mode = LO_COLOR; Style::from(Color::Dark(BaseColor::from((color_cmd - 90) as u8))) },
-            100..=107 => { mode = LO_COLOR; Style::from(Color::Dark(BaseColor::from((color_cmd - 100) as u8))) },
+            90..=97 => { mode = LO_COLOR; Style::from(Color::Light(BaseColor::from((color_cmd - 90) as u8))).combine(Effect::Bold) },
+            100..=107 => { mode = LO_COLOR; Style::from(Color::Light(BaseColor::from((color_cmd - 100) as u8))).combine(Effect::Bold) },
             _ => style
         };
 
         style = Style::merge(&vec!(style, next_style));
     }
 
-    println!("{:?}", style);
     style
 }
 
